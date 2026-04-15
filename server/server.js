3b868fa2dafc83f2de16fc2c535963b981decdb7
alizen1107 <aliozen1107@gmail.com>
2026-04-15 20:31:03 +0300

    mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 const app = express();
+
 
 //middleware
 app.use(cors());
@@ -22,8 +23,28 @@ app.use(express.json());
 
 app.post("/test", (req, res) => {
     const {name} = req.body.name;
-    
+
     res.send(name);
 
 });
 
+
+
+// MongoDB bağlantısı
+mongoose.connect("mongodb://127.0.0.1:27017/votingapp")
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err);
+});
+
+// test route
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+// server
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
